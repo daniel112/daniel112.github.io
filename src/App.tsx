bd9a632a47109c385a0535c9ca2b66cb2d1c3132
Daniel Yo <danielyo112@gmail.com>
2024-10-18 17:38:48 -0700

    chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Tr,
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex id="main" height={"100vh"} width={"100vw"}>
+      <Center flexDir={"column"} flex={1}>
+        <Button width={"fit-content"}>Click me </Button>
+
+        <Table variant={"striped"} width={500}>
+          <Tbody>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <Tr key={index}>
+                {Array.from({ length: 8 }).map((_, index) => (
+                  <Td key={index}>
+                    <Center>
+                      <Text>{index}</Text>
+                    </Center>
+                  </Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Center>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
