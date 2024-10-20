316bbac1ec266b01affcc06fe80b00ff6e3563ae
Daniel Yo <danielyo112@gmail.com>
2024-10-20 12:17:30 -0700

    reset

--- src/Pages/RootPage.tsx
+++ src/Pages/RootPage.tsx
@@ -1,17 +1,10 @@
 import { Box, Button } from "@chakra-ui/react";
 import { FC } from "react";
-import { useFoo } from "../store/reducers/fooReducer";
 
 export const RootPage: FC = () => {
-  const {
-    foo,
-    actions: { setFoo },
-  } = useFoo();
   return (
     <Box>
-      <Button colorScheme="blue" onClick={() => setFoo("TEST")}>
-        Click me {foo.value}
-      </Button>
+      <Button colorScheme="blue">Click me</Button>
     </Box>
   );
 };
